862dc903ee374b072536d513250ac2899bbb4163
XXRG456 <139345557+XXRG456@users.noreply.github.com>
2023-07-21 11:24:08 +0100

    Bokeh interactive plots

--- bokehplot.py
+++ bokehplot.py
@@ -2,23 +2,42 @@
 import numpy as np
 from bokeh.io import curdoc
 from bokeh.layouts import column, row
-from bokeh.models import ColumnDataSource, Slider, TextInput
+from bokeh.models import ColumnDataSource, Slider, TextInput, CustomJS
 from bokeh.plotting import figure, show
+from bokeh.io import output_notebook
 
-# Set up data
-N = 400
-x = np.linspace(0, 4*np.pi, N)
+x = np.linspace(0, 10, 500)
 y = np.sin(x)
+
 source = ColumnDataSource(data=dict(x=x, y=y))
 
-
-# Set up plot
-plot = figure(height=400, width=400, title="my sine wave",
-              tools="crosshair,pan,reset,save,wheel_zoom",
-              x_range=[0, 4*np.pi], y_range=[-2.5, 2.5])
+plot = figure(y_range=(-10, 10), width=400, height=400)
 
 plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
-show(plot)
+
+amp = Slider(start=0.1, end=10, value=1, step=.1, title="Amplitude")
+freq = Slider(start=0.1, end=10, value=1, step=.1, title="Frequency")
+phase = Slider(start=-6.4, end=6.4, value=0, step=.1, title="Phase")
+offset = Slider(start=-9, end=9, value=0, step=.1, title="Offset")
+
+callback = CustomJS(args=dict(source=source, amp=amp, freq=freq, phase=phase, offset=offset),
+                    code="""
+    const A = amp.value
+    const k = freq.value
+    const phi = phase.value
+    const B = offset.value
+
+    const x = source.data.x
+    const y = Array.from(x, (x) => B + A*Math.sin(k*x+phi))
+    source.data = { x, y }
+""")
+
+amp.js_on_change('value', callback)
+freq.js_on_change('value', callback)
+phase.js_on_change('value', callback)
+offset.js_on_change('value', callback)
+output_notebook()
+show(row(plot, column(amp, freq, phase, offset)))
 
 from bokeh.layouts import gridplot
 from bokeh.models import CDSView, ColumnDataSource, IndexFilter
